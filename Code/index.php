74dcd5fec4a12d36d67a9d717d27c65f2cd4b3aa
JKtech <jktech@post.cz>
2023-11-23 10:29:17 +0100

    Design update

---
+++ Code/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Srsťoplsť</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/index.js"></script>
+</head>
+<body>
+    <div id="content-container">
+        <nav id="nav">
+            <ul>
+                <li class="active">
+                    <a href="#">
+                        <i class="bi bi-chat-left-text"></i> 
+                        <span>Články</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="bi bi-newspaper"></i> 
+                        <span>Vydání</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="bi bi-person"></i>
+                        <span>Účet</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="bi bi-info-circle"></i>
+                        <span>O nás</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+        <main id="page-content">
+            <div id="content-header">
+                <h3>Články</h3>
+            </div>
+        </main>
+
+        <div id="authors-container">
+            <div id="search-box">
+                <input id="search" type="search" />
+            </div>
+        </div>
+    </div>
+</body>
+</html>
