ffb67045971ee6571415d0f08a2ce6793a8290ea
JKtech <jktech@post.cz>
2023-12-21 21:43:07 +0100

    Searching articles fix

--- Code/search/index.php
+++ Code/search/index.php
@@ -25,8 +25,6 @@
             SELECT articles.*, CONCAT(users.firstname, ' ', users.lastname) AS author, users.login, IF(users.avatar IS NULL, 'default.png', users.avatar) AS avatar
             FROM articles
             INNER JOIN users ON articles.author = users.userID
-            RIGHT JOIN article_tag ON article_tag.article = articles.articleID
-            LEFT JOIN tags ON tags.tagID = article_tag.tag
             WHERE articles.title LIKE ?
             ORDER BY articles.articleID DESC
         ", "%" . $query . "%");
